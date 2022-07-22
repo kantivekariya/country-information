02feb9382596c939b2f47b6fc2e4a1d32748570e
kantivekariya <kantivekariya242@gmail.com>
2022-07-23 00:06:00 +0530

    rm createSelector

--- src/redux/features/countryInfoDetailsSlice.tsx
+++ src/redux/features/countryInfoDetailsSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice, createSelector, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CountryDetailsInfo {
   countryDetailsInfo: Array<any>;
--- src/redux/features/countryInfoSlice.tsx
+++ src/redux/features/countryInfoSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice, createSelector, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CountryInfo {
   countryList: Array<any>;
